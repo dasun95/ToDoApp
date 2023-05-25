46786583cff149049f8608d6c3918aeabca37550
Dasun95 <dasunweerasinghe95@gmail.com>
2023-05-25 22:08:31 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./myday/myday.module').then((m) => m.MydayModule) },
+  { path: 'myday', loadChildren: () => import('./myday/myday.module').then((m) => m.MydayModule) },
   { path: 'planned', loadChildren: () => import('./planned/planned.module').then((m) => m.PlannedModule) },
   { path: 'assigned', loadChildren: () => import('./assigned/assigned.module').then((m) => m.AssignedModule) },
   { path: 'tasks', loadChildren: () => import('./tasks/tasks.module').then((m) => m.TasksModule) },
